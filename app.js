bef40de9097c10dc86dcedb08270a764ddecb89c
ranjayspices-sys <ranjayspices@gmail.com>
2026-06-03 14:21:40 +1100

    Create app.js

---
+++ app.js
@@ -0,0 +1,58 @@
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+function addToCart(name, price) {
+  cart.push({ name, price });
+  localStorage.setItem("cart", JSON.stringify(cart));
+  alert(name + " added");
+}
+
+function renderCart() {
+  let container = document.getElementById("cartItems");
+  let total = 0;
+  container.innerHTML = "";
+
+  cart.forEach(item => {
+    total += item.price;
+    let div = document.createElement("div");
+    div.innerHTML = item.name + " - ₹" + item.price;
+    container.appendChild(div);
+  });
+
+  document.getElementById("total").innerText = "Total: ₹" + total;
+  return total;
+}
+
+function payNow() {
+  let total = renderCart();
+
+  var options = {
+    "key": "YOUR_RAZORPAY_KEY_ID",
+    "amount": total * 100,
+    "currency": "INR",
+    "name": "Ranjay Spices",
+    "description": "Spice Order Payment",
+    "handler": function (response) {
+      alert("Payment Successful: " + response.razorpay_payment_id);
+      sendOrderToSheet(response.razorpay_payment_id);
+      sendWhatsAppConfirmation();
+    },
+    "prefill": {
+      "name": "",
+      "email": "",
+      "contact": ""
+    },
+    "theme": {
+      "color": "#8B0000"
+    }
+  };
+
+  var rzp = new Razorpay(options);
+  rzp.open();
+}
+
+function sendWhatsAppConfirmation() {
+  let msg = "Order Confirmed from Ranjay Spices:%0A";
+  cart.forEach(i => msg += i.name + "%0A");
+
+  window.open("https://wa.me/91XXXXXXXXXX?text=" + msg, "_blank");
+}
